b84fa8ca8efa06d0d1439e471e8ef18a7ccfcd51
Toby Stanislaus <tstanislaus@denseair.net>
2024-07-15 17:30:33 +0100

    almost there with finishing the data extraction

--- netbox.py
+++ netbox.py
@@ -1,12 +1,35 @@
 import requests
+import urllib3
 
-url="https://netbox-dev.da.int/api/"
-payload={}
-headers = {
-    'Content-Type':'application/json',
-    'Authorization':'Token 2b9ab16a20cc1d8cd5cc73039d3c818711a0fb35'
-}
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+url="https://netbox-dev.da.int/api/dcim/devices/9/"
+token="2b9ab16a20cc1d8cd5cc73039d3c818711a0fb35"
+desiredKeys=['region','site','location','rack','position','tenant','device_type','description','airflow','serial','asset_tag','config_template']
 
-response=requests.request('GET',url,headers=headers,data=payload)
+def fetchData(url,token):
+    payload={}
+    headers = {
+        'Content-Type':'application/json',
+        'Authorization':'Token '+token
+        }
+    response=requests.request('GET',url,headers=headers,data=payload, verify=False)
+    
+    response=fetchData(url,token)
+    data=response.json()
+    specifyData(desiredKeys,data,token)
 
-print(response.json())
+
+def specifyData(desiredKeys,data,token):
+    result=[]
+    for key in data:
+        if key in desiredKeys:
+   
+            if isinstance(data[key],dict):
+                try:
+                    print(fetchData(data[key]['url'],token).json())
+                except:
+                    pass
+    return result
+
+
+
