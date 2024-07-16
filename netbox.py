ab7d733d28a667b084ac43a692d08869addbce55
Toby Stanislaus <tstanislaus@denseair.net>
2024-07-16 08:15:29 +0100

    retrieving raw data from APIs

--- netbox.py
+++ netbox.py
@@ -4,32 +4,30 @@ import urllib3
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 url="https://netbox-dev.da.int/api/dcim/devices/9/"
 token="2b9ab16a20cc1d8cd5cc73039d3c818711a0fb35"
-desiredKeys=['region','site','location','rack','position','tenant','device_type','description','airflow','serial','asset_tag','config_template']
+desiredKeys=['site','location','rack','position','tenant','device_type','description','airflow','serial','asset_tag','config_template']
+headers = {
+    'Content-Type':'application/json',
+    'Authorization':'Token '+token
+    }
 
-def fetchData(url,token):
+def fetchData(url,headers):
     payload={}
-    headers = {
-        'Content-Type':'application/json',
-        'Authorization':'Token '+token
-        }
     response=requests.request('GET',url,headers=headers,data=payload, verify=False)
-    
-    response=fetchData(url,token)
-    data=response.json()
-    specifyData(desiredKeys,data,token)
+    return response.json()
 
 
-def specifyData(desiredKeys,data,token):
-    result=[]
-    for key in data:
-        if key in desiredKeys:
-   
-            if isinstance(data[key],dict):
-                try:
-                    print(fetchData(data[key]['url'],token).json())
-                except:
-                    pass
-    return result
 
+def findCorrectData(dataDict,headers):
+
+    return dataDict[key]
+
+dataDict=fetchData(url,headers)
+
+resultDict={}
+for key in desiredKeys:
+
+    data=findCorrectData(dataDict,headers)
+    resultDict[key]=data
+    print(data)
 
 
