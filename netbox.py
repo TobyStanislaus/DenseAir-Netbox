cabff1831a32ca26143d55f2287296a11fedefad
Toby Stanislaus <tstanislaus@denseair.net>
2024-07-16 11:53:18 +0100

    finished project

--- netbox.py
+++ netbox.py
@@ -2,8 +2,8 @@ import requests
 import urllib3
 
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
-url="https://netbox-dev.da.int/api/dcim/"
-token=""
+url="https://netbox-dev.da.int/api/dcim/devices/9/"
+token="ba3ac42a21fc1a55aeede388c446a0929f16fe99"
 desiredKeys=['site','location','rack','position','tenant','device_type','description','airflow','serial','asset_tag','config_template']
 headers = {
     'Content-Type':'application/json',
@@ -15,18 +15,18 @@ def fetchData(url,headers):
     response=requests.request('GET',url,headers=headers,data=payload, verify=False)
     return response.json()
 
-
-
-def findCorrectData(dataDict,headers,key):  
+def findCorrectData(dataDict,url,headers,key):  
     #print(key)
     #print(dataDict[key])
-    if isinstance(dataDict[key],dict) and 'url' in dataDict[key]:
+   
+    if (isinstance(dataDict[key],dict) and 'url' in dataDict[key]):
         result=findRawDictData(dataDict[key]['url'],headers)
-        #print(key)
-        #print(dataDict[key]['url'])
-        #print(result)
         return result
-        
+    
+    if isinstance(dataDict[key],str) and dataDict[key][:8]=='https://' and dataDict[key]!=url:
+        result=findRawDictData(dataDict[key],headers)
+        return result
+    
     else:
         return dataDict[key]
 
@@ -37,12 +37,11 @@ def findRawDictData(url,headers):
 
     for key in dataDict:
 
-        data=findCorrectData(dataDict,headers,key)
+        data=findCorrectData(dataDict,url,headers,key)
         resultDict[key]=data
 
     return resultDict
 
-fetchData(url,headers)
 resultDict=findRawDictData(url,headers)
-print('a')
+print(resultDict)
 
